741ce17c4950cefa8d191b7c0b81b893ddd1cff3
kristykawai <kristy.kw.chan@gmail.com>
2024-05-03 23:09:17 +0930

    Added html content

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$e^b@kh-&nn+f48m58v&8)p(vzg)u-50*rkop_9y$zq)--x9g+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
